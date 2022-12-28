21f71d37efd5e6146af8d79f5529290e450774fc
SOURAVMONDAL-BD <souravmondalcode@gmail.com>
2022-12-28 17:36:58 +0600

    user get post put

--- index.js
+++ index.js
@@ -16,9 +16,67 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
+async function run(){
+
+   try{
+
+
+      const usersCollection = client.db('trendify').collection('users');
 
 
 
+
+
+      app.get('/users/:id', async (req, res) => {
+         const email = req.params.id;
+         const query = { email: email };
+         const cursor = await usersCollection.find(query);
+         const user = await cursor.toArray();
+
+         res.send(user);
+     })
+
+
+
+      app.post('/users', async (req, res) => {
+         const user = req.body;
+         const result = await usersCollection.insertOne(user);
+         res.send(result);
+     })
+
+
+
+     app.put('/users/:id', async (req, res) => {
+      const email = req.params.id;
+      const query = { email: email };
+      const user = req.body;
+      console.log(user.email);
+      const options = { upsert: true };
+      const updatedUser = {
+          $set: {
+              name: user.name,
+              email: user.email,
+          }
+      }
+      if (user.email) {
+          const result = await usersCollection.updateOne(query, updatedUser, options);
+          res.send(result);
+      }
+
+  })
+
+
+
+   }
+   finally{
+
+   }
+
+
+}
+
+
+run().catch(err => console.error(err));
 
 
 app.get('/', async(req, res) => {
